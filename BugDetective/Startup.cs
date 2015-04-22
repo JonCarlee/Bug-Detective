e30c3b03f703a33163c1ac423abd537d311bb375
JonCarlee <joncarlee@gmail.com>
2015-04-22 11:15:31 -0400

    Initial Commit
    Starting Over

---
+++ BugDetective/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BugDetective.Startup))]
+namespace BugDetective
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
